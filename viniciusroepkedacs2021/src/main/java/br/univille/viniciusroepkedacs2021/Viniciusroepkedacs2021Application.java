2c626d5a2a6c681a8351cbb237b061bf9fe57263
viniciusroepke <vinicius.roepke3@gmail.com>
2021-06-01 23:51:17 +0000

    primeira aula

---
+++ viniciusroepkedacs2021/src/main/java/br/univille/viniciusroepkedacs2021/Viniciusroepkedacs2021Application.java
@@ -0,0 +1,13 @@
+package br.univille.viniciusroepkedacs2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Viniciusroepkedacs2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Viniciusroepkedacs2021Application.class, args);
+	}
+
+}
---
+++ viniciusroepkedacs2021/src/test/java/br/univille/viniciusroepkedacs2021/Viniciusroepkedacs2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.viniciusroepkedacs2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Viniciusroepkedacs2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
